417f770d57ab614bdb6643de19650472126661de
Chris Smith <ole088@gmail.com>
2024-03-23 19:32:59 -0400

    day4 complete

---
+++ day4/solution4.py
@@ -0,0 +1,50 @@
+# aoc_template.py
+
+import pathlib
+import sys
+from hashlib import md5
+
+def parse(puzzle_input: str) -> list:
+    """Parse text input"""
+    return puzzle_input
+
+def part1(data: list) -> str:
+    """Solve part 1"""
+    integer = 0
+    while True:
+        hashtest = data + str(integer)
+        md5sum = md5(hashtest.encode('utf-8')).hexdigest()
+        if md5sum.startswith('00000'):
+            break
+        else:
+            integer += 1
+
+    return integer
+
+def part2(data: list) -> str:
+    """Solve part 2"""
+    integer = 0
+    while True:
+        hashtest = data + str(integer)
+        md5sum = md5(hashtest.encode('utf-8')).hexdigest()
+        if md5sum.startswith('000000'):
+            break
+        else:
+            integer += 1
+
+    return integer
+
+def solve(puzzle_input: str) -> list:
+    """Solve the puzzle for the given input"""
+    data = parse(puzzle_input=puzzle_input)
+    solution1 = f"Part 1: {part1(data=data)}"
+    solution2 = f"Part 2: {part2(data=data)}"
+
+    return solution1, solution2
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        print(f"{path}:")
+        puzzle_input = pathlib.Path(path).read_text().strip()
+        solutions = solve(puzzle_input=puzzle_input)
+        print("\n".join(str(solution) for solution in solutions))
