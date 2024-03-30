d1874af957042392b1024998a413dae8da239af8
Chris Smith <ole088@gmail.com>
2024-03-30 18:44:28 -0400

    set pythonic comment spacing add line comments

--- day3/solution3.py
+++ day3/solution3.py
@@ -5,10 +5,12 @@ import sys
 
 def parse(puzzle_input: str) -> list:
     """Parse text input"""
+
     return puzzle_input
 
 def add_house(x: int, y: int, coordinates: list):
     """Create a house coordinate"""
+
     house = (x, y)
     # if the house is not in coordinates, add it
     if house not in coordinates:
@@ -16,65 +18,87 @@ def add_house(x: int, y: int, coordinates: list):
 
 def part1(data: list) -> str:
     """Solve part 1"""
+
     startX, startY = 0, 0
     coords = []
+    # add the starting coordinate
     add_house(startX, startY, coords)
 
+    # loop over directions
     for dir in data:
+        # move north
         if dir == '^':
             startY += 1
+        # move south
         elif dir == 'v':
             startY -= 1
+        # move west
         elif dir == '<':
             startX -=1
+        # move east
         elif dir == '>':
             startX += 1
 
+        # add the newest coordinate
         add_house(startX, startY, coords)
 
     return len(coords)
 
 def part2(data: list) -> str:
     """Solve part 2"""
+
     santaX, santaY = 0, 0
     roboX, roboY = 0, 0
     coords = []
     santa_turn = True
+    # add the starting coordinate
     add_house(santaX, santaY, coords)
 
+    # loop over the directions
     for dir in data:
+        # move north
         if dir == '^':
+            # update position based on whose turn it is
             if santa_turn:
                 santaY += 1
             else:
                 roboY += 1
+        # move south
         elif dir == 'v':
+            # update position based on whose turn it is
             if santa_turn:
                 santaY -= 1
             else:
                 roboY -= 1
+        # move west
         elif dir == '<':
+            # update position based on whose turn it is
             if santa_turn:
                 santaX -=1
             else:
                 roboX -=1
+        # move east
         elif dir == '>':
+            # update position based on whose turn it is
             if santa_turn:
                 santaX += 1
             else:
                 roboX += 1
 
+        # add the coordinate to the list based on whose turn it is
         if santa_turn:
             add_house(santaX, santaY, coords)
         else:
             add_house(roboX, roboY, coords)
         
+        # swap turns
         santa_turn = not santa_turn
 
     return len(coords)
 
 def solve(puzzle_input: str) -> list:
     """Solve the puzzle for the given input"""
+
     data = parse(puzzle_input=puzzle_input)
     solution1 = f"Part 1: {part1(data=data)}"
     solution2 = f"Part 2: {part2(data=data)}"
