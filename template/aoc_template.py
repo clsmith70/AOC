d1474a68b58e931025c762e4f0b324f548ca71f4
Chris Smith <ole088@gmail.com>
2024-03-30 18:31:51 -0400

    set pythonic comment spacing

--- template/aoc_template.py
+++ template/aoc_template.py
@@ -5,6 +5,7 @@ import sys
 
 def parse(puzzle_input: str) -> list:
     """Parse text input"""
+    
     return [line for line in puzzle_input.split()]
 
 def part1(data: list) -> str:
@@ -15,6 +16,7 @@ def part2(data: list) -> str:
 
 def solve(puzzle_input: str) -> list:
     """Solve the puzzle for the given input"""
+
     data = parse(puzzle_input=puzzle_input)
     solution1 = f"Part 1: {part1(data=data)}"
     solution2 = f"Part 2: {part2(data=data)}"
