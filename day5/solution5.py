ab8de91412cf335195c2f8a6f5ac67522f6e09e7
Chris Smith <ole088@gmail.com>
2024-03-30 18:57:42 -0400

    set pythonic comment spacing add line comments

--- day5/solution5.py
+++ day5/solution5.py
@@ -6,13 +6,19 @@ import sys
 
 def parse(puzzle_input: str) -> list:
     """Parse text input"""
+
     return [line for line in puzzle_input.split()]
 
 def get_char_count(string: str, matchset: str):
     """Get count of characters in a matchset"""
-    string = string.casefold() # ensure the entire string is lowercase
+    
+    # ensure the entire string is lowercase
+    string = string.casefold()
+    # create a dict of matchset chars with initial count of 0
     count = {}.fromkeys(matchset, 0)
+    # loop over characters and count them
     for character in string:
+        # as long as they are in the matchset
         if character in count:
             count[character] += 1
     
@@ -20,21 +26,32 @@ def get_char_count(string: str, matchset: str):
 
 def check_nice(string: str):
     """Check for part2 niceness"""
+
+    # string is not nice if there aren't at least 2 
+    # occurrences of the same double
     if not any([string[i] == string[i + 2] for i in range(len(string) - 2)]):
         return False
-    if any([(string.count(string[i: i + 2]) >= 2) for i in range(len(string) -2)]):
+    # string is nice if it includes one alternating set of chars
+    # ex: aba or xmx
+    if any([(string.count(string[i: i + 2]) >= 2) \
+            for i in range(len(string) -2)]):
         return True
     return False
 
 def part1(data: list) -> str:
     """Solve part 1"""
+
     nice_count = 0
     regexA = re.compile(r"(.)\1")
     regexB = re.compile('ab|cd|pq|xy')
 
+    # loop over the lines
     for line in data:
+        # rule: string must contain at least 3 vowels
         if get_char_count(line, 'aeiou') >= 3:
+            # rule: the string must have at least one double
             if regexA.findall(line):
+                # rule: the string does not contain any illegal groups
                 if not regexB.findall(line):
                     nice_count += 1
 
@@ -42,16 +59,21 @@ def part1(data: list) -> str:
 
 def part2(data: list) -> str:
     """Solve part 2"""
+
     nice_count = 0
 
+    # loop over the lines
     for line in data:
+        # if they are nice
         if check_nice(line):
+            # count them
             nice_count += 1
 
     return nice_count
 
 def solve(puzzle_input: str) -> list:
     """Solve the puzzle for the given input"""
+
     data = parse(puzzle_input=puzzle_input)
     solution1 = f"Part 1: {part1(data=data)}"
     solution2 = f"Part 2: {part2(data=data)}"
--- day5/test_solution5.py
+++ day5/test_solution5.py
@@ -18,16 +18,20 @@ def example2():
 
 def test_parse_example1(example1):
     """Test that input is parsed properly."""
+    
     assert example1 == ['ugknbfddgicrmopn','aaa','jchzalrnumimnmhp','haegwjzuvuyypxyu','dvszwmarrgswjxmb']
 
 def test_part1_example1(example1):
     """Test part 1 on example input."""
+    
     assert aoc.part1(example1) == 2
 
 def test_part2_example1(example1):
     """Test part 2 on example input."""
+    
     assert aoc.part2(example1) == 0
 
 def test_part2_example2(example2):
     """Test part 2 on example input."""
+    
     assert aoc.part2(example2) == 2
