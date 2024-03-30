287d356c5dbc3de0d272e80f4c1a8b96298a9bb1
Chris Smith <ole088@gmail.com>
2024-03-30 09:03:26 -0400

    day11 complete

---
+++ day11/solution11.py
@@ -0,0 +1,75 @@
+# aoc_template.py
+
+import pathlib
+import re
+import sys
+
+VALID_CHARS = 'abcdefghjkmnpqrstuvwxyz'
+
+def parse(puzzle_input: str) -> list:
+    """Parse text input"""
+    return [line for line in puzzle_input.split('\n')]
+
+def test_password(password: str) -> bool:
+    """Test password validity"""
+    valid_pairs = [char + char for char in VALID_CHARS]
+    straights = [''.join(c) for c in zip(VALID_CHARS[:-2], VALID_CHARS[1:-1], VALID_CHARS[2:])]
+
+    # RULE1: cannot contain i, l, or o
+    invalid_chars = r"ilo"
+    if any(elem in password for elem in invalid_chars): return False
+    # RULE2: must contain at least 2 double character sets
+    if sum([pair in password for pair in valid_pairs]) < 2: return False
+    # RULE3: must contain a straight of 3 or more consecutive valid characters
+    if not any([run in password for run in straights]): return False
+
+    return True
+
+def next_password(password: str) -> str:
+    """Find the next password"""
+    next_letter = {char1: char2 for char1, char2 in zip(VALID_CHARS, VALID_CHARS[1:]+'a')}
+    # add invalid chars to assure they are dealt with properly
+    next_letter['i'] = 'j'
+    next_letter['l'] = 'm'
+    next_letter['o'] = 'p'
+
+    password = password[:-1] + next_letter[password[-1]]
+
+    for i in range(-1, -8, -1):
+        if password[i] == 'a':
+            password = password[:i - 1] + next_letter[password[i - 1]] + password[i:]
+        else:
+            break
+        
+    return password
+
+def part1(data: list) -> str:
+    """Solve part 1"""
+    password = data[0]
+    while test_password(password) == False:
+        password = next_password(password)
+    
+    return password
+
+def part2(data: str) -> str:
+    """Solve part 2"""
+    password = next_password(data)
+    while test_password(password) == False:
+        password = next_password(password)
+    
+    return password
+
+def solve(puzzle_input: str) -> list:
+    """Solve the puzzle for the given input"""
+    data = parse(puzzle_input=puzzle_input)
+    solution1 = f"Part 1: {part1(data=data)}"
+    solution2 = f"Part 2: {part2(data=solution1.split(': ')[1])}"
+
+    return solution1, solution2
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        print(f"{path}:")
+        puzzle_input = pathlib.Path(path).read_text().strip()
+        solutions = solve(puzzle_input=puzzle_input)
+        print("\n".join(str(solution) for solution in solutions))
---
+++ day11/test_solution11.py
@@ -0,0 +1,35 @@
+# test_aoc_template.py
+
+import pathlib
+import pytest
+import solution11 as aoc
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+@pytest.fixture
+def example1():
+    puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
+    return aoc.parse(puzzle_input)
+
+@pytest.fixture
+def example2():
+    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
+    return aoc.parse(puzzle_input)
+
+def test_parse_example1(example1: list):
+    """Test that input is parsed properly."""
+    assert example1 == ['hijklmmn','abbceffg','abbcegjk']
+
+def test_part1_example1(example1: list):
+    """Test password validity on example input."""
+    assert aoc.test_password(example1[0]) == False
+    assert aoc.test_password(example1[1]) == False
+    assert aoc.test_password(example1[2]) == False
+
+def test_part1_example2(example2: list):
+    """Test part 1 on example input."""
+    assert aoc.part1([example2[0]]) == 'abcdffaa'
+    assert aoc.part1([example2[1]]) == 'ghjaaabb' #'ghjaabcc' is wrong
+    # a key error occurs when i, l, or o are encountered
+    # to address this, they have to be added to the next_letter dict
+    # when added, they appropriately adjust what the next valid password is
