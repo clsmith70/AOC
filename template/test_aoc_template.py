389c21eddc21baefc0860c661fad91c7070a95e6
Chris Smith <ole088@gmail.com>
2024-03-30 13:10:36 -0400

    Add type annotations

--- template/test_aoc_template.py
+++ template/test_aoc_template.py
@@ -17,21 +17,21 @@ def example2():
     return aoc.parse(puzzle_input)
 
 @pytest.mark.skip(reason="Not implemented")
-def test_parse_example1(example1):
+def test_parse_example1(example1: list):
     """Test that input is parsed properly."""
     assert example1 == ...
 
 @pytest.mark.skip(reason="Not implemented")
-def test_part1_example1(example1):
+def test_part1_example1(example1: list):
     """Test part 1 on example input."""
     assert aoc.part1(example1) == ...
 
 @pytest.mark.skip(reason="Not implemented")
-def test_part2_example1(example1):
+def test_part2_example1(example1: list):
     """Test part 2 on example input."""
     assert aoc.part2(example1) == ...
 
 @pytest.mark.skip(reason="Not implemented")
-def test_part2_example2(example2):
+def test_part2_example2(example2: list):
     """Test part 2 on example input."""
     assert aoc.part2(example2) == ...
