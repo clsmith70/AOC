e4d3e2ba5ea48aa887ba31fb03a37b23e1902bf4
Chris Smith <ole088@gmail.com>
2024-03-30 12:45:45 -0400

    reformat parse test assertion

--- day13/test_solution13.py
+++ day13/test_solution13.py
@@ -13,12 +13,18 @@ def example1():
 
 def test_parse_example1(example1):
     """Test that input is parsed properly."""
-    assert example1 == ['Alice would gain 54 happiness units by sitting next to Bob.','Alice would lose 79 happiness units by sitting next to Carol.',
-                        'Alice would lose 2 happiness units by sitting next to David.','Bob would gain 83 happiness units by sitting next to Alice.',
-                        'Bob would lose 7 happiness units by sitting next to Carol.','Bob would lose 63 happiness units by sitting next to David.',
-                        'Carol would lose 62 happiness units by sitting next to Alice.','Carol would gain 60 happiness units by sitting next to Bob.',
-                        'Carol would gain 55 happiness units by sitting next to David.','David would gain 46 happiness units by sitting next to Alice.',
-                        'David would lose 7 happiness units by sitting next to Bob.','David would gain 41 happiness units by sitting next to Carol.']
+    assert example1 == ['Alice would gain 54 happiness units by sitting next to Bob.',
+                        'Alice would lose 79 happiness units by sitting next to Carol.',
+                        'Alice would lose 2 happiness units by sitting next to David.',
+                        'Bob would gain 83 happiness units by sitting next to Alice.',
+                        'Bob would lose 7 happiness units by sitting next to Carol.',
+                        'Bob would lose 63 happiness units by sitting next to David.',
+                        'Carol would lose 62 happiness units by sitting next to Alice.',
+                        'Carol would gain 60 happiness units by sitting next to Bob.',
+                        'Carol would gain 55 happiness units by sitting next to David.',
+                        'David would gain 46 happiness units by sitting next to Alice.',
+                        'David would lose 7 happiness units by sitting next to Bob.',
+                        'David would gain 41 happiness units by sitting next to Carol.']
 
 def test_part1_example1(example1):
     """Test part 1 on example input."""
