1b3295f9f9e0918987ccd62b4548164f23e33755
Chris Smith <ole088@gmail.com>
2024-03-30 19:44:39 -0400

    set pythonic line lengths set pythonic comment spacing add line comments

--- day11/solution11.py
+++ day11/solution11.py
@@ -8,59 +8,80 @@ VALID_CHARS = 'abcdefghjkmnpqrstuvwxyz'
 
 def parse(puzzle_input: str) -> list:
     """Parse text input"""
+
     return [line for line in puzzle_input.split('\n')]
 
 def test_password(password: str) -> bool:
     """Test password validity"""
+
     valid_pairs = [char + char for char in VALID_CHARS]
-    straights = [''.join(c) for c in zip(VALID_CHARS[:-2], VALID_CHARS[1:-1], VALID_CHARS[2:])]
+    straights = [''.join(c) for c in \
+                 zip(VALID_CHARS[:-2], VALID_CHARS[1:-1], VALID_CHARS[2:])]
 
     # RULE1: cannot contain i, l, or o
     invalid_chars = r"ilo"
     if any(elem in password for elem in invalid_chars): return False
     # RULE2: must contain at least 2 double character sets
     if sum([pair in password for pair in valid_pairs]) < 2: return False
-    # RULE3: must contain a straight of 3 or more consecutive valid characters
+    # RULE3: must contain a straight of 3 or more consecutive valid chars
     if not any([run in password for run in straights]): return False
 
     return True
 
 def next_password(password: str) -> str:
     """Find the next password"""
-    next_letter = {char1: char2 for char1, char2 in zip(VALID_CHARS, VALID_CHARS[1:]+'a')}
+
+    next_letter = {char1: char2 for char1, char2 in \
+                   zip(VALID_CHARS, VALID_CHARS[1:]+'a')}
+    
     # add invalid chars to assure they are dealt with properly
     next_letter['i'] = 'j'
     next_letter['l'] = 'm'
     next_letter['o'] = 'p'
 
+    # increment the last letter to initialize
     password = password[:-1] + next_letter[password[-1]]
 
+    # loop over the chars from right to left, incrementing as needed
     for i in range(-1, -8, -1):
+        # if the current letter is 'a', increment
         if password[i] == 'a':
-            password = password[:i - 1] + next_letter[password[i - 1]] + password[i:]
+            password = password[:i - 1] + \
+                next_letter[password[i - 1]] + \
+                password[i:]
         else:
+            # otherwise stop
             break
         
     return password
 
 def part1(data: list) -> str:
     """Solve part 1"""
+
+    # get the first item in the list (should only be one)
     password = data[0]
+    # as long as the password is not valid
     while test_password(password) == False:
+        # get the next one
         password = next_password(password)
     
     return password
 
 def part2(data: str) -> str:
     """Solve part 2"""
+
+    # get the passed in password string
     password = next_password(data)
+    # as long as the password is not valid
     while test_password(password) == False:
+        # get the next one
         password = next_password(password)
     
     return password
 
 def solve(puzzle_input: str) -> list:
     """Solve the puzzle for the given input"""
+    
     data = parse(puzzle_input=puzzle_input)
     solution1 = f"Part 1: {part1(data=data)}"
     solution2 = f"Part 2: {part2(data=solution1.split(': ')[1])}"
--- day11/test_solution11.py
+++ day11/test_solution11.py
@@ -18,18 +18,21 @@ def example2():
 
 def test_parse_example1(example1: list):
     """Test that input is parsed properly."""
+
     assert example1 == ['hijklmmn','abbceffg','abbcegjk']
 
 def test_part1_example1(example1: list):
     """Test password validity on example input."""
+
     assert aoc.test_password(example1[0]) == False
     assert aoc.test_password(example1[1]) == False
     assert aoc.test_password(example1[2]) == False
 
 def test_part1_example2(example2: list):
     """Test part 1 on example input."""
+
     assert aoc.part1([example2[0]]) == 'abcdffaa'
     assert aoc.part1([example2[1]]) == 'ghjaaabb' #'ghjaabcc' is wrong
     # a key error occurs when i, l, or o are encountered
     # to address this, they have to be added to the next_letter dict
-    # when added, they appropriately adjust what the next valid password is
+    # when added, they properly adjust what the next valid password is
