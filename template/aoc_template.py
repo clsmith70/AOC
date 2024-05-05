1b60b04022e2b18e6b897dee4ec117d912678183
Chris Smith <ole088@gmail.com>
2024-05-05 14:02:32 -0400

    Correct function usage

--- template/aoc_template.py
+++ template/aoc_template.py
@@ -24,6 +24,6 @@ def solve(puzzle_input):
 if __name__ == "__main__":
     for path in sys.argv[1:]:
         print(f"{path}:")
-        puzzle_input = pathlib.Path(path).read_text.strip()
+        puzzle_input = pathlib.Path(path).read_text().strip()
         solutions = solve(puzzle_input=puzzle_input)
         print("\n".join(str(solution) for solution in solutions))
