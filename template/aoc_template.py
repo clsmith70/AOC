0e4e281e191f5d0217b9595d7ac5b99430d6e351
Chris Smith <ole088@gmail.com>
2024-12-01 13:47:13 -0500

    Update template code

--- template/aoc_template.py
+++ template/aoc_template.py
@@ -3,17 +3,19 @@
 import pathlib
 import sys
 
-def parse(puzzle_input):
+def parse(puzzle_input: str):
     """Parse text input"""
-    return [line for line in puzzle_input.split()]
+    return list(puzzle_input.split('\n'))
 
-def part1(data):
+def part1(data: list):
     """Solve part 1"""
+    return data
 
-def part2(data):
+def part2(data: list):
     """Solve part 2"""
+    return data
 
-def solve(puzzle_input):
+def solve(puzzle_input: str):
     """Solve the puzzle for the given input"""
     data = parse(puzzle_input=puzzle_input)
     solution1 = part1(data)
@@ -24,6 +26,6 @@ def solve(puzzle_input):
 if __name__ == "__main__":
     for path in sys.argv[1:]:
         print(f"{path}:")
-        puzzle_input = pathlib.Path(path).read_text.strip()
+        puzzle_input = pathlib.Path(path).read_text(encoding='utf-8').strip()
         solutions = solve(puzzle_input=puzzle_input)
         print("\n".join(str(solution) for solution in solutions))
