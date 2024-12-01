ec358f5e3642e06f3abb1a00412427aa21e72a68
Chris Smith <ole088@gmail.com>
2024-12-01 13:53:56 -0500

    Add docstrings and address recommendations

--- template/test_aoc_template.py
+++ template/test_aoc_template.py
@@ -8,30 +8,32 @@ PUZZLE_DIR = pathlib.Path(__file__).parent
 
 @pytest.fixture
 def example1():
+    """Parse the first example input file and return that result"""
     puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
     return aoc.parse(puzzle_input)
 
 @pytest.fixture
 def example2():
+    """Parse the second example input file and return that result"""
     puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
     return aoc.parse(puzzle_input)
 
 @pytest.mark.skip(reason="Not implemented")
-def test_parse_example1(example1):
+def test_parse_example1(example=example1):
     """Test that input is parsed properly."""
-    assert example1 == ...
+    assert example == ...
 
 @pytest.mark.skip(reason="Not implemented")
-def test_part1_example1(example1):
+def test_part1_example1(example=example1):
     """Test part 1 on example input."""
-    assert aoc.part1(example1) == ...
+    assert aoc.part1(example) == ...
 
 @pytest.mark.skip(reason="Not implemented")
-def test_part2_example1(example1):
+def test_part2_example1(example=example1):
     """Test part 2 on example input."""
-    assert aoc.part2(example1) == ...
+    assert aoc.part2(example) == ...
 
 @pytest.mark.skip(reason="Not implemented")
-def test_part2_example2(example2):
+def test_part2_example2(example=example2):
     """Test part 2 on example input."""
-    assert aoc.part2(example2) == ...
+    assert aoc.part2(example) == ...
