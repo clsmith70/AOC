6deea76673a4b00e294e4f16b1b29a4ba4a0b507
Chris Smith <ole088@gmail.com>
2024-12-03 12:33:42 -0500

    Update fixture names to follow pylint docs

--- template/test_aoc_template.py
+++ template/test_aoc_template.py
@@ -6,34 +6,34 @@ import aoc_template as aoc
 
 PUZZLE_DIR = pathlib.Path(__file__).parent
 
-@pytest.fixture
+@pytest.fixture(name="data1")
 def example1():
     """Parse the first example input file and return that result"""
     puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
     return aoc.parse(puzzle_input)
 
-@pytest.fixture
+@pytest.fixture(name="data2")
 def example2():
     """Parse the second example input file and return that result"""
     puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
     return aoc.parse(puzzle_input)
 
 @pytest.mark.skip(reason="Not implemented")
-def test_parse_example1(example=example1):
+def test_parse_example1(data1):
     """Test that input is parsed properly."""
-    assert example == ...
+    assert data1 == ...
 
 @pytest.mark.skip(reason="Not implemented")
-def test_part1_example1(example=example1):
+def test_part1_example1(data1):
     """Test part 1 on example input."""
-    assert aoc.part1(example) == ...
+    assert aoc.part1(data1) == ...
 
 @pytest.mark.skip(reason="Not implemented")
-def test_part2_example1(example=example1):
+def test_part2_example1(data1):
     """Test part 2 on example input."""
-    assert aoc.part2(example) == ...
+    assert aoc.part2(data1) == ...
 
 @pytest.mark.skip(reason="Not implemented")
-def test_part2_example2(example=example2):
+def test_part2_example2(data2):
     """Test part 2 on example input."""
-    assert aoc.part2(example) == ...
+    assert aoc.part2(data2) == ...
