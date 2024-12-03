c412280bc3459318ccdd6a23fb820ccfba84dacf
Chris Smith <ole088@gmail.com>
2024-12-03 12:29:27 -0500

    Update pytest script to follow pylint docs

--- day2/test_solution.py
+++ day2/test_solution.py
@@ -6,20 +6,20 @@ import solution as aoc
 
 PUZZLE_DIR = pathlib.Path(__file__).parent
 
-@pytest.fixture
+@pytest.fixture(name="data")
 def example1():
     """Parse the first example input file and return that result"""
     puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
     return aoc.parse(puzzle_input)
 
-def test_parse_example1(example1):
+def test_parse_example1(data):
     """Test that input is parsed properly."""
-    assert example1[0] == [7, 6, 4, 2, 1]
+    assert data[0] == [7, 6, 4, 2, 1]
 
-def test_part1_example1(example1):
+def test_part1_example1(data):
     """Test part 1 on example input."""
-    assert aoc.part1(example1) == 2
+    assert aoc.part1(data) == 2
 
-def test_part2_example1(example1):
+def test_part2_example1(data):
     """Test part 2 on example input."""
-    assert aoc.part2(example1) == 4
+    assert aoc.part2(data) == 4
